2ebcae2c95007a27cfe2abbb44c52ad45d361f4e
saruhirthick <43338229+saruhirthick@users.noreply.github.com>
2019-02-07 14:27:09 +0530

    Create prgrm14.c

---
+++ prgrm14.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int rem,i,intr1,intr2;
+clrscr();
+printf("enter the two intervals:\n");
+scanf("%d,%d",&intr1,intr2);
+printf("The odd numbers between %d and %d are:\n",intr1,intr2);
+for(i=intr1;i<=intr2;i++)
+{
+rem=i%2;
+if(rem!=0)
+{
+printf("\n%d",i);
+}
+}
+getch();
+}
