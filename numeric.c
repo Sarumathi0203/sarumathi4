a8da9e5a3e80a522b0f0da5020c5b1fd40c1f322
saruhirthick <43338229+saruhirthick@users.noreply.github.com>
2019-03-05 12:22:28 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int isdigit = 0;
+int j = 0;
+char temp[16];
+clrscr();
+printf("Enter a valid string:\n");
+scanf("%s",temp)
+while(j<strlen(temp)  && isDigit == 0)
+{
+if(temp[j]>57 && temp[i]<48)
+{
+isDigit = 0;
+}
+else
+{
+isDigit =1;
+}
+j++;
+}
+return 0;
+}
